390e56777474838ab6513e84b8f2c33963d9a86c
MarketaHorakova <99902734+MarketaHorakova@users.noreply.github.com>
2023-11-19 21:33:45 +0100

    Update adding item - add to file directly

--- Program.cs
+++ Program.cs
@@ -4,22 +4,6 @@ string pathStorage = "C:\\Users\\marketa.zemlova\\Visual Studio 2023\\C sharp 2\
 
 
 List<Clothing> skrin = new List<Clothing>();
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "S", "beige", "cotton", 95, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "M", "beige", "cotton", 98, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "L", "beige", "cotton", 102, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "XL", "beige", "cotton", 105, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "2XL", "beige", "cotton", 108, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "3XL", "beige", "cotton", 112, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1350, 700, "4XL", "beige", "cotton", 116, "circle shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "S", "beige", "polyester", 100, "V shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "M", "beige", "polyester", 100, "V shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "L", "beige", "polyester", 105, "V shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "XL", "beige", "polyester", 105, "V shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "2XL", "beige", "polyester", 110, "V shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "3XL", "beige", "polyester", 110, "V shape", true));
-skrin.Add(new Tshirt("Tshirt", "Tricko telove", 0, 1650, 900, "4XL", "beige", "polyester", 110, "V shape", true));
-skrin.Add(new Shirt("Shirt", "Kosile bila", 0, 1090, 600, "XL", "white", "cotton", 105, true));
-skrin.Add(new Shirt("Shirt", "Kosile ivory",0, 1450, 800, "XL", "ivory", "cotton", 110, true));
 
 bool isOver = false;
 
@@ -33,8 +17,7 @@ while (!isOver)
         "3 - Prodej položek\n" +
         "4 - Naskladni polozky\n" +
         "5 - Zapis do souboru\n" +
-        "6 - Nacteni ze souboru\n" +
-        "7 - Inventura");
+        "6 - Inventura");
     int.TryParse(Console.ReadLine(), out int choosenNumberMenu);
 
     switch (choosenNumberMenu)
@@ -43,7 +26,7 @@ while (!isOver)
         case 0:
             isOver = true;
             break;
-        //Dictionary to console
+        //Pridani polozky
         case 1:
             Console.WriteLine("Kterou položku:\n" +
                  "0 - Tričko pod košili\n" +
@@ -66,17 +49,23 @@ while (!isOver)
             string material = Console.ReadLine();
             bool discount = false;
             
+            string textFromFile = File.ReadAllText(pathStorage);
+            string[] lines = textFromFile.Split('\n');
+            int i = lines.Length;
 
             switch (choosenNumberItem)
             {
                 case 0:
-                    skrin.Add(new Tshirt("Tshirt",nameInput, piecesToStorage, priceSell, priceBuy, size, color, material, 95, "V shape", true));                   
+                    //skrin.Add(new Tshirt("Tshirt",nameInput, piecesToStorage, priceSell, priceBuy, size, color, material, 95, "V shape", true));
+                    File.AppendAllText(pathStorage, $"\n{"Tshirt"},{i},{nameInput},{piecesToStorage},{priceSell},{priceBuy},{discount},{size},{color},{material}");
                     break;
                 case 1:
-                    skrin.Add(new Shirt("Shirt",nameInput, piecesToStorage, priceSell, priceBuy, size, color, material, 105, true));                    
+                    //skrin.Add(new Shirt("Shirt",nameInput, piecesToStorage, priceSell, priceBuy, size, color, material, 105, true));
+                    File.AppendAllText(pathStorage, $"\n{"Tshirt"},{i},{nameInput},{piecesToStorage},{priceSell},{priceBuy},{discount},{size},{color},{material}");
                     break;
                 case 2:
-                    skrin.Add(new Accessories("Accessories",nameInput, piecesToStorage, priceSell, priceBuy,  size, color, material,false, "Tie"));                    
+                    //skrin.Add(new Accessories("Accessories",nameInput, piecesToStorage, priceSell, priceBuy,  size, color, material,false, "Tie"));                    
+                    File.AppendAllText(pathStorage, $"\n{"Accessories"},{i},{nameInput},{piecesToStorage},{priceSell},{priceBuy},{discount},{size},{color},{material}");
                     break;
             }
 
@@ -103,7 +92,7 @@ while (!isOver)
         
         // Zapis do souboru
         case 5:
-            int i = 0;
+            i = 0;
             File.Delete(pathStorage);
 
             foreach (var item in skrin)
@@ -144,25 +133,15 @@ while (!isOver)
                 i++;
             }
             break;
-        // Nacteni ze souboru
+        // Inventura
         case 6:
-            string textFromFile = File.ReadAllText(pathStorage);
-            string[] lines = textFromFile.Split('\n');
-                       
+            //nacteni ze souboru
+            textFromFile = File.ReadAllText(pathStorage);
+            lines = textFromFile.Split('\n');
+
             foreach (var line in lines)
             {
                 string[] oneItem = line.Split(',');
-
-                Console.WriteLine($"index 0: {oneItem[0]}");
-                Console.WriteLine($"index 1: {oneItem[1]}");
-                Console.WriteLine($"index 2: {oneItem[2]}");
-                Console.WriteLine($"index 3: {oneItem[3]}");
-                Console.WriteLine($"index 4: {oneItem[4]}");
-                Console.WriteLine($"index 5: {oneItem[5]}");
-                Console.WriteLine($"index 6: {oneItem[6]}");
-                Console.WriteLine($"index 7: {oneItem[7]}");
-                Console.WriteLine($"index 8: {oneItem[8]}");
-                Console.WriteLine($"index 9: {oneItem[9]}");
 
                 id = int.Parse(oneItem[1]);
                 nameInput = oneItem[2];
@@ -188,6 +167,7 @@ while (!isOver)
                 }
             }
 
+            // Vypis do konzole
             i = 0;
             Console.WriteLine("ID, Nazev, Na sklade, Kupni cena, Prodejni cena, Sleva, Velikost, Hlavni barva, Material");
             foreach (var item in skrin)
@@ -196,17 +176,7 @@ while (!isOver)
                 i++;
             }
             break;
-        // Inventura
-        case 7:
-            i = 0;
-            Console.WriteLine("ID, Nazev, Na sklade, Kupni cena, Prodejni cena, Sleva, Velikost, Hlavni barva, Material");
-            foreach (var item in skrin)
-            {
-                Console.WriteLine($"{i},{skrin[i].Name},{skrin[i].PiecesOnStock},{skrin[i].PriceSell},{skrin[i].PriceBuy},{skrin[i].Discount},{skrin[i].Size},{skrin[i].ColorMain},{skrin[i].Material}");
-                i++;
-            }
 
-            break;
     }
 
 }
